514f42e8aa632174a08275a1b923547d7ec9f2b7
Rich1115 <111610219+Rich1115@users.noreply.github.com>
2022-09-11 17:08:53 -0500

    Create userSerializer.py

---
+++ WeldingDragonApp/Serializers/userSerializer.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from WeldingDragon.Models.User import User 
+
+class UserSerializer(serializers.ModelSerializer):
+  
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'nombre', 'apellido', 'cedula', 'correo', 'tipo_de_Cliente']
+
+    def create(self, validated_data):
+        accountData = validated_data.pop('account')
+        userInstance = User.objects.create(**validated_data)
+        return userInstance
+
+    def to_representation(self, obj):
+        user = User.objects.get(id=obj.id)
+        return {
+            'id': user.id,
+            'username': user.username,
+            'nombre': user.nombre,
+            'apellido': user.apellido,
+            'cedula': user.cedula,
+            'correo': user.correo,
+            'tipo_de_Cliente': user.tipo_de_Cliente,
+            
+}
