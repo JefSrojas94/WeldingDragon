850ab39723aad2aac7746d8e4971971b2418e60b
JeffryRojas <jeffrysnneyder.rr@gmail.com>
2022-09-11 17:40:22 -0500

    Se actualiza nombre de la carptera weldApp a weldProject, y se modifican archivos que importen la carpera con el nuevo nombre

--- weldProject/asgi.py
+++ weldProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for weldApp project.
+ASGI config for weldProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weldApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weldProject.settings')
 
 application = get_asgi_application()
